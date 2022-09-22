01b5914f0f75b71e048c6ae2e58b0bf6708ad70a
Steven Tidwell <steventid1313@gmail.com>
2022-09-21 19:18:11 -0500

    Completed Exercises Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Photos';
+  image1 = 'https://i.pinimg.com/originals/6c/de/7a/6cde7accc735d48c66abef4a170590a3.jpg';
+  image2 = 'https://i0.wp.com/www.aesdes.org/wp-content/uploads/2020/01/Cyberpunk-1.jpg';
+  image3 = 'https://www.looper.com/img/gallery/the-history-of-tatooine-fully-explained/intro-1598632525.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'Little Mermaid', 'The Purge'];
 
    constructor() { }
 
