26989a18a306e8d09d118ddef571da43da7d694b
Steven Tidwell <steventid1313@gmail.com>
2022-09-21 20:10:29 -0500

    Completed Parts 1-3 and Optional Final Touches

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+
+  favLinks = ['https://www.google.com', 'https://steventid.github.io'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,6 +10,7 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://i.pinimg.com/originals/6c/de/7a/6cde7accc735d48c66abef4a170590a3.jpg';
   image2 = 'https://i0.wp.com/www.aesdes.org/wp-content/uploads/2020/01/Cyberpunk-1.jpg';
   image3 = 'https://www.looper.com/img/gallery/the-history-of-tatooine-fully-explained/intro-1598632525.jpg';
+  image4 = 'https://i.giphy.com/media/scZPhLqaVOM1qG4lT9/giphy.webp';
 
   constructor() { }
 
